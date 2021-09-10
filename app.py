c96bda80918139c649cce21e4545611c10c5bf82
GodwinAN <89367409+GodwinAN@users.noreply.github.com>
2021-09-10 17:59:15 -0400

    updated app

--- app.py
+++ app.py
@@ -2,9 +2,10 @@
 # A number is even if division by 2 gives a remainder of 0.
 # If the remainder is 1, it is an odd number.
 
-"""docstring"""
-num = int(input("Enter a number: "))
-if (num % 2) == 0:
-    print("{0} is Even".format(num))
-else:
-    print("{0} is Odd".format(num))
+def oddoreven(num):
+    if (num % 2) == 0:
+        print("{0} is Even".format(num))
+    else:
+        print("{0} is Odd".format(num))
+        pass
+oddoreven(565)
