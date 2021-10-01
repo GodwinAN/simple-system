7264a834842200057ffd8bfefb1ce2e1b6e4f459
GodwinAN <89367409+GodwinAN@users.noreply.github.com>
2021-10-01 16:00:04 -0400

    Update main.py

--- main.py
+++ main.py
@@ -11,19 +11,6 @@ def oddoreven(num: int):
                 return{"{0}".format(num): "is Even"}
             else:
                 return{"{0}".format(num): "is Odd"}
-               
-
-#@app.get("/")
-#async def root():
-#    return {"message": "Hello World"}
-
-#@app.get("/test")
-#async def goddy_funct():
-#    return {"message": "Hello Godwin"}
-
-#@#app.get("/items/{item_id}")
-#def read_item(item_id: int, q: Optional[str] = None):
- #   return {"item_id": item_id, "q": q}
 
 
 if __name__ == '__main__':
